e1f771500d02a50625952e8bfb53532ae85d3389
erick <erickgmbarron@gmail.com>
2024-08-02 16:19:23 -0700

    [dev] mvp is working

--- alienware_project/Domain/DTOs/branches/BranchDTO.cs
+++ alienware_project/Domain/DTOs/branches/BranchDTO.cs
@@ -6,8 +6,9 @@ namespace Domain.DTOs.branches
 	public class BranchDTO
 	{
 		public string BranchId { get; set; } = string.Empty;
-		public string Name { get; set; } = string.Empty;
-		public string Description { get; set; } = string.Empty;
+		public string LogoUrl { get; set; } = string.Empty;
+        public string Name { get; set; } = string.Empty;
+		public List<string> Descriptions { get; set; } = new List<string>();
         public string PhoneNumber { get; set; } = string.Empty;
         public StatusBranch Status { get; set; }
 	}
